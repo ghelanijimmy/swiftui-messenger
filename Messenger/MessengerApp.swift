e3d4cb1ae3d2118216ac4b1023db5d321dedb283
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-10-25 23:09:37 -0400

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Jimmy Ghelani on 2023-10-25.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
